1fb20fa9a257e007633c87dbebd5825b5586750e
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-07-13 17:20:55 +0600

    delete operation is added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -42,6 +42,15 @@ async function run() {
 
     app.get('/users', async(req,res)=> {
         const result = await usersCollection.find().toArray()
+        res.send(result)
+
+    })
+
+    app.delete('/users/:id', async(req,res)=> {
+
+        const id = req.params.id 
+        const query = { _id : new ObjectId(id)}
+        const result = await usersCollection.deleteOne(query)
         res.send(result)
 
     })
